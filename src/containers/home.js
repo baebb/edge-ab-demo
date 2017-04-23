7e5c25d403f379a6cb4eb7fac23b2e1308c9299b
Ross Dyson <dyson.ross@gmail.com>
2017-04-23 12:00:02 +1000

    Setting up A and B site

--- src/containers/home.js
+++ src/containers/home.js
@@ -8,7 +8,9 @@ class Home extends React.Component {
       <Grid>
         <Row>
           <Col xs={12} sm={6} smOffset={3}>
-            <h1 className="text-center">edge-ab-demo</h1>
+            <h1 className="text-center">A/B testing @ edge demo</h1>
+            <h4 className="text-center site-version">A</h4>
+            <h4 className="text-center">Site</h4>
           </Col>
         </Row>
       </Grid>
